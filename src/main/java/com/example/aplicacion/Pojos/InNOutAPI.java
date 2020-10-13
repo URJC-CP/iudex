ec4a40b950efeaf1fd0cec1b2115f8bbf4ae3de8
pavloXd <palopa07@gmail.com>
2020-10-13 22:08:05 +0200

    problem API entity

---
+++ src/main/java/com/example/aplicacion/Pojos/InNOutAPI.java
@@ -0,0 +1,7 @@
+package com.example.aplicacion.Pojos;
+
+public class InNOutAPI {
+    private long id;
+    private String text;
+
+}
--- src/main/java/com/example/aplicacion/Pojos/ProblemAPI.java
+++ src/main/java/com/example/aplicacion/Pojos/ProblemAPI.java
@@ -10,9 +10,9 @@ public class ProblemAPI {
 
     private String nombreEjercicio;
 
-    private List<InNOut> entradaVisible;
-    private List<InNOut>  salidaOculta;
-    private ProblemEntradaSalidaVisiblesHTML ejemplos;
+    private List<String> entradaVisible;
+    private List<String>  salidaVisible;
+
 
     private List<SubmissionAPI> submissions;
 
@@ -28,26 +28,9 @@ public class ProblemAPI {
     private String source_url;
     private String license;
     private String rights_owner;
-    //private String keywords;
-    private String hashString;
-    //Marca si el problema es publico o no
-    private boolean disponible;
 
-    private byte[] documento;
 
-    private String validation;
-    private String validation_flags;
 
-    private String limit_time_multiplier;
-    private String limit_time_safety_margin;
-    private String limit_memory;
-    private String limit_output;
-    private String limit_code;
-    private String limit_compilation_time;
-    private String limit_compilation_memory;
-    private String limit_validation_time;
-    private String limit_validation_memory;
-    private String limit_validation_output;
     private String color;
 
     public long getId() {
@@ -66,29 +49,6 @@ public class ProblemAPI {
         this.nombreEjercicio = nombreEjercicio;
     }
 
-    public List<InNOut> getEntradaVisible() {
-        return entradaVisible;
-    }
-
-    public void setEntradaVisible(List<InNOut> entradaVisible) {
-        this.entradaVisible = entradaVisible;
-    }
-
-    public List<InNOut> getSalidaOculta() {
-        return salidaOculta;
-    }
-
-    public void setSalidaOculta(List<InNOut> salidaOculta) {
-        this.salidaOculta = salidaOculta;
-    }
-
-    public ProblemEntradaSalidaVisiblesHTML getEjemplos() {
-        return ejemplos;
-    }
-
-    public void setEjemplos(ProblemEntradaSalidaVisiblesHTML ejemplos) {
-        this.ejemplos = ejemplos;
-    }
 
     public List<SubmissionAPI> getSubmissions() {
         return submissions;
@@ -170,124 +130,20 @@ public class ProblemAPI {
         this.rights_owner = rights_owner;
     }
 
-    public String getHashString() {
-        return hashString;
+    public List<String> getEntradaVisible() {
+        return entradaVisible;
     }
 
-    public void setHashString(String hashString) {
-        this.hashString = hashString;
+    public void setEntradaVisible(List<String> entradaVisible) {
+        this.entradaVisible = entradaVisible;
     }
 
-    public boolean isDisponible() {
-        return disponible;
+    public List<String> getSalidaVisible() {
+        return salidaVisible;
     }
 
-    public void setDisponible(boolean disponible) {
-        this.disponible = disponible;
-    }
-
-    public byte[] getDocumento() {
-        return documento;
-    }
-
-    public void setDocumento(byte[] documento) {
-        this.documento = documento;
-    }
-
-    public String getValidation() {
-        return validation;
-    }
-
-    public void setValidation(String validation) {
-        this.validation = validation;
-    }
-
-    public String getValidation_flags() {
-        return validation_flags;
-    }
-
-    public void setValidation_flags(String validation_flags) {
-        this.validation_flags = validation_flags;
-    }
-
-    public String getLimit_time_multiplier() {
-        return limit_time_multiplier;
-    }
-
-    public void setLimit_time_multiplier(String limit_time_multiplier) {
-        this.limit_time_multiplier = limit_time_multiplier;
-    }
-
-    public String getLimit_time_safety_margin() {
-        return limit_time_safety_margin;
-    }
-
-    public void setLimit_time_safety_margin(String limit_time_safety_margin) {
-        this.limit_time_safety_margin = limit_time_safety_margin;
-    }
-
-    public String getLimit_memory() {
-        return limit_memory;
-    }
-
-    public void setLimit_memory(String limit_memory) {
-        this.limit_memory = limit_memory;
-    }
-
-    public String getLimit_output() {
-        return limit_output;
-    }
-
-    public void setLimit_output(String limit_output) {
-        this.limit_output = limit_output;
-    }
-
-    public String getLimit_code() {
-        return limit_code;
-    }
-
-    public void setLimit_code(String limit_code) {
-        this.limit_code = limit_code;
-    }
-
-    public String getLimit_compilation_time() {
-        return limit_compilation_time;
-    }
-
-    public void setLimit_compilation_time(String limit_compilation_time) {
-        this.limit_compilation_time = limit_compilation_time;
-    }
-
-    public String getLimit_compilation_memory() {
-        return limit_compilation_memory;
-    }
-
-    public void setLimit_compilation_memory(String limit_compilation_memory) {
-        this.limit_compilation_memory = limit_compilation_memory;
-    }
-
-    public String getLimit_validation_time() {
-        return limit_validation_time;
-    }
-
-    public void setLimit_validation_time(String limit_validation_time) {
-        this.limit_validation_time = limit_validation_time;
-    }
-
-    public String getLimit_validation_memory() {
-        return limit_validation_memory;
-    }
-
-    public void setLimit_validation_memory(String limit_validation_memory) {
-        this.limit_validation_memory = limit_validation_memory;
-    }
-
-    public String getLimit_validation_output() {
-        return limit_validation_output;
-    }
-
-    public void setLimit_validation_output(String limit_validation_output) {
-        this.limit_validation_output = limit_validation_output;
+    public void setSalidaVisible(List<String> salidaVisible) {
+        this.salidaVisible = salidaVisible;
     }
 
     public String getColor() {
