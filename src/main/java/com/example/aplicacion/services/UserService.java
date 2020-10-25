ab04d59805020286ef2fbd4342e93ff2d9165964
pavloXd <palopa07@gmail.com>
2020-10-25 22:07:52 +0100

    +

--- src/main/java/com/example/aplicacion/services/UserService.java
+++ src/main/java/com/example/aplicacion/services/UserService.java
@@ -2,6 +2,7 @@ package com.example.aplicacion.services;
 
 import com.example.aplicacion.Entities.Team;
 import com.example.aplicacion.Entities.User;
+import com.example.aplicacion.Pojos.UserString;
 import com.example.aplicacion.Repository.LanguageRepository;
 import com.example.aplicacion.Repository.TeamRepository;
 import com.example.aplicacion.Repository.UserRepository;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -68,6 +70,34 @@ public class UserService {
             return "OK";
         }
 
+    }
+
+    public UserString updateUser(String userId, Optional<String> nickname, Optional<String> mail){
+        UserString userString = new UserString();
+        User user = userRepository.findUserById(Long.valueOf(userId));
+        if(user==null){
+            userString.setSalida("USER NOT FOUND");
+            return userString;
+        }
+
+        if(nickname.isPresent()){
+            if(!(userRepository.findByNickname(nickname.get()) == null)){
+                userString.setSalida("USER ALREADY IN USE");
+                return userString;
+            }
+            else {
+                user.setNickname(nickname.get());
+                userRepository.save(user);
+            }
+        }
+        if(mail.isPresent()){
+            user.setEmail(mail.get());
+        }
+
+        userRepository.save(user);
+        userString.setSalida("OK");
+        userString.setUser(user);
+        return userString;
     }
 
     public User getUserById(long userId){
