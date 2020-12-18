3b44d2a68aeb65367a2054e83d2c8082107db661
Sourius <sourius@users.noreply.github.com>
2020-12-18 17:05:38 +0100

    Update log in BasicController

--- src/main/java/com/example/aplicacion/Controllers/standarControllers/BasicController.java
+++ src/main/java/com/example/aplicacion/Controllers/standarControllers/BasicController.java
@@ -40,44 +40,42 @@ public class BasicController {
 
     @PostConstruct
     public void init() {
-        logger.info("Building Java language image ");
+        logger.info("Build Java language image ");
         File dckfl = new File("DOCKERS/Java/Dockerfile");
         String imageId = resultHandler.buildImage(dckfl);
         Language lenguaje = new Language("java", imageId);
         languageRepository.save(lenguaje);
-        logger.info("Built Java language image " + imageId + " from " + dckfl.getName());
+        logger.info("Finish build Java language image " + imageId + " from " + dckfl.getName());
 
         //Creamos el lenguaje Python
-        logger.info("Building Python language image");
+        logger.info("Build Python language image");
         File dckfl2 = new File("DOCKERS/Python3/Dockerfile");
         String imageId2 = resultHandler.buildImage(dckfl2);
         Language lenguaje2 = new Language("python3", imageId2);
         languageRepository.save(lenguaje2);
-        logger.info("Built Python language image " + imageId2 + " from " + dckfl2.getName());
+        logger.info("Finish build Python language image " + imageId2 + " from " + dckfl2.getName());
 
         //Creamos el lenguaje C
-        logger.info("Building C language image");
+        logger.info("Build language image");
         File dckfl3 = new File("DOCKERS/C/Dockerfile");
         String imageId3 = resultHandler.buildImage(dckfl3);
         Language lenguaje3 = new Language("c", imageId3);
         languageRepository.save(lenguaje3);
-        logger.info("Built C language image " + imageId3 + " from " + dckfl3.getName());
+        logger.info("Finish build C language image " + imageId3 + " from " + dckfl3.getName());
 
         //Creamos el lenguaje CPP
-        logger.info("Building C++ language image");
+        logger.info("Build C++ language image");
         File dckfl4 = new File("DOCKERS/CPP/Dockerfile");
         String imageId4 = resultHandler.buildImage(dckfl4);
         Language lenguaje4 = new Language("cpp", imageId4);
         languageRepository.save(lenguaje4);
-        logger.info("Built C++ language image " + imageId4 + " from " + dckfl4.getName());
+        logger.info("Finish build C++ language image " + imageId4 + " from " + dckfl4.getName());
 
         userService.crearUsuario("pavloXd", "mail1");
         //userService.crearUsuario("pavloXD", "mail2");
         //userService.crearUsuario("pavloXD2", "mail1");
         //userService.deleteUserByNickname("pavloXD");
 
-        logger.info("Building contest");
         contestService.creaContest("contestPrueba", Long.toString(teamService.getTeamByNick("pavloXd").getId()), Optional.of("Este es el mejor concurso del mundo"));
-        logger.info("Built contest");
     }
 }
