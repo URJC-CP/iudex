0bc0cda1cc8cf93a0ee06d389aa47afad9deb42e
Sourius <sourius@users.noreply.github.com>
2020-12-18 17:01:50 +0100

    add log to ContestService

--- src/main/java/com/example/aplicacion/services/ContestService.java
+++ src/main/java/com/example/aplicacion/services/ContestService.java
@@ -8,6 +8,8 @@ import com.example.aplicacion.Pojos.ProblemAPI;
 import com.example.aplicacion.Repository.ContestRepository;
 import com.example.aplicacion.Repository.ProblemRepository;
 import com.example.aplicacion.Repository.TeamRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -28,9 +30,13 @@ public class ContestService {
     @Autowired
     private ProblemService problemService;
 
+    Logger logger = LoggerFactory.getLogger(ContestService.class);
+
     public ContestString creaContest(String nameContest, String teamId, Optional<String> descripcion){
+        logger.info("Build contest "+nameContest+"\nTeam: "+teamId+"\nDescription: "+descripcion);
         ContestString salida = new ContestString();
         if(contestRepository.existsByNombreContest(nameContest)){
+            logger.error("Contest name duplicated "+nameContest);
             salida.setSalida("contest NAME DUPLICATED");
             return salida;
         }
@@ -43,6 +49,7 @@ public class ContestService {
         }
         Team team =teamRepository.findTeamById(Long.valueOf(teamId));
         if (team == null) {
+            logger.error("Team "+teamId+" not found");
             salida.setSalida("TEAM NOT FOUND");
             return salida;
         }
@@ -52,14 +59,17 @@ public class ContestService {
 
         salida.setSalida("OK");
         salida.setContest(contest);
+        logger.debug("Finish build contest "+nameContest+"\nId: "+contest.getId());
         return salida;
     }
 
     public ContestString updateContest(String contestId, Optional<String> nameContest, Optional<String> teamId, Optional<String> descripcion){
+        logger.info("Update contest "+contestId);
         ContestString salida = new ContestString();
         //Buscamos el contest
         Contest contest = contestRepository.findContestById(Long.valueOf(contestId));
         if (contest == null){
+            logger.error("Contest "+contestId+" with name "+nameContest+" doesn't exist");
             salida.setSalida("CONTEST ID DOES NOT EXIST");
             return salida;
         }
@@ -67,6 +77,7 @@ public class ContestService {
         //Si namecontest esta presente lo cambiamos
         if(nameContest.isPresent()){
             if(contestRepository.existsByNombreContest(nameContest.get())){
+                logger.error("Contest name "+nameContest.get()+" duplicated");
                 salida.setSalida("CONTEST NAME DUPLICATED");
                 return salida;
             }
@@ -76,6 +87,7 @@ public class ContestService {
         if (teamId.isPresent()){
             Team team =teamRepository.findTeamById(Long.valueOf(teamId.get()));
             if (team == null) {
+                logger.error("Team "+teamId.get()+" not found");
                 salida.setSalida("TEAM NOT FOUND");
                 return salida;
             }
@@ -88,13 +100,16 @@ public class ContestService {
 
         salida.setSalida("OK");
         salida.setContest(contest);
+        logger.debug("Finish update contest "+contestId);
         return salida;
     }
 
 
     public String deleteContest(String idcontest){
+        logger.info("Delete contest "+idcontest);
         Contest contest = contestRepository.findContestById(Long.valueOf(idcontest));
         if(contest ==null){
+            logger.error("Contest "+idcontest+" not found");
             return "contest NOT FOUND";
         }
         /*
@@ -115,83 +130,97 @@ public class ContestService {
 
         //borramos el contest
         contestRepository.delete(contest);
-
+        logger.debug("Finish delete contest "+idcontest);
         return "OK";
     }
 
     public String anyadeProblemaContest(String idContest, String idProblema){
-
+        logger.info("Add problem "+idProblema+" to contest "+idContest);
         Contest contest = contestRepository.findContestById(Long.valueOf(idContest));
         Problem problema = problemRepository.findProblemById(Long.valueOf(idProblema));
         if(contest ==null){
+            logger.error("Contest "+idContest+" not found");
             return "contest NOT FOUND";
         }
         if (problema==null){
-            return "USER NOT FOUND";
+            logger.error("Problem "+idProblema+" not found");
+            return "PROBLEM NOT FOUND";
         }
         if(contest.getListaProblemas().contains(problema)){
+            logger.error("Problem "+idProblema+" already in contest");
             return "PROBLEM DUPLICATED";
         }
         contest.addProblem(problema);
         contestRepository.save(contest);
-
+        logger.debug("Finish add problem "+idProblema+" to contest "+idContest);
         return "OK";
     }
 
     public String deleteProblemFromContest(String idContest, String idProblema){
-
+        logger.info("Delete problem "+idProblema+" from contest "+idContest);
         Contest contest = contestRepository.findContestById(Long.valueOf(idContest));
         Problem problema = problemRepository.findProblemById(Long.valueOf(idProblema));
         if(contest ==null){
+            logger.error("Contest "+idContest+" not found");
             return "contest NOT FOUND";
         }
         if (problema==null){
-            return "USER NOT FOUND";
+            logger.error("Problem "+idProblema+" not found");
+            return "PROBLEM NOT FOUND";
         }
         if(!contest.getListaProblemas().contains(problema)){
+            logger.error("Problem "+idProblema+" not in contest "+idContest);
             return "PROBLEM NOT IN CONCURSO";
         }
         contest.deleteProblem(problema);
         contestRepository.save(contest);
 
-
+        logger.debug("Finish delete problem "+idProblema+" from contest "+idContest);
         return "OK";
     }
 
     public String addTeamTocontest(String idTeam, String idcontest){
+        logger.info("Add team/user "+idTeam+" to contest "+idcontest);
         Contest contest = contestRepository.findContestById(Long.valueOf(idcontest));
         Team team = teamRepository.findTeamById(Long.valueOf(idTeam));
         if(contest ==null){
+            logger.error("Contest "+idcontest+" not found");
             return "contest NOT FOUND";
         }
         if (team==null){
+            logger.error("Team/user "+idTeam+" not found");
             return "USER NOT FOUND";
         }
         else {
             if(!contest.getListaParticipantes().contains(team)){
                 contest.addTeam(team);
                 contestRepository.save(contest);
-
             }
             else {
+                logger.error("Team/user "+idTeam+" already in contest "+idcontest);
                 return "YA ESTA EN EL CONCURSO";
             }
         }
+        logger.debug("Finish add team/user "+idTeam+" to contest "+idcontest);
         return "OK";
     }
 
 
     public String deleteTeamFromcontest(String idcontest, String idTeam){
+        logger.info("Delete team/user "+idTeam+" from contest "+idcontest);
         Contest contest = contestRepository.findContestById(Long.valueOf(idcontest));
         Team team = teamRepository.findTeamById(Long.valueOf(idTeam));
         if(contest ==null){
+            logger.error("Contest "+idcontest+" not found");
             return "contest NOT FOUND";
         }
         if (team==null){
+            logger.error("Team/user "+idTeam+" not found");
             return "USER NOT FOUND";
         }
         else {
             if(!contest.getListaParticipantes().contains(team)){
+                logger.error("Team/user "+idTeam+" not in contest "+idcontest);
                 return "NO ESTA EN EL CONCURSO";
             }
             else {
@@ -199,6 +228,7 @@ public class ContestService {
                 contestRepository.save(contest);
             }
         }
+        logger.debug("Finish delete team/user "+idTeam+" from contest "+idcontest);
         return "OK";
     }
 
