3252f5f82dcd84b983828ef19c33690485b6ff69
Sourius <sourius@users.noreply.github.com>
2021-01-04 17:19:07 +0100

    remove junit5 from pom

--- src/test/java/com/example/aplicacion/smoketest/SmokeTest.java
+++ src/test/java/com/example/aplicacion/smoketest/SmokeTest.java
@@ -8,19 +8,17 @@ import com.example.aplicacion.Controllers.standarControllers.BasicController;
 import com.example.aplicacion.Controllers.standarControllers.ContestController;
 import com.example.aplicacion.Controllers.standarControllers.IndiceController;
 import com.example.aplicacion.Controllers.standarControllers.ProblemController;
-import com.example.aplicacion.Entities.Result;
 import com.example.aplicacion.Repository.*;
 import com.example.aplicacion.rabbitMQ.RabbitResultExecutionReceiver;
 import com.example.aplicacion.rabbitMQ.RabbitResultExecutionSender;
 import com.example.aplicacion.rabbitMQ.RabbitResultRevieserReceiver;
 import com.example.aplicacion.rabbitMQ.RabbitResultReviserSender;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class SmokeTest {
@@ -76,68 +74,63 @@ public class SmokeTest {
     @Test
     public void contextLoads() {
         //check rabbit
-        Assertions.assertNotNull(reviserReceiver);
-        Assertions.assertNotNull(receiver);
-        Assertions.assertNotNull(sender);
-        Assertions.assertNotNull(reviserSender);
+        assertNotNull(reviserReceiver);
+        assertNotNull(receiver);
+        assertNotNull(sender);
+        assertNotNull(reviserSender);
 
         //check api controllers have been initialized
-        Assertions.assertNotNull(apiContestController);
-        Assertions.assertNotNull(apiProblemController);
-        Assertions.assertNotNull(apiSubmissionController);
-        Assertions.assertNotNull(apiTeamController);
+        assertNotNull(apiContestController);
+        assertNotNull(apiProblemController);
+        assertNotNull(apiSubmissionController);
+        assertNotNull(apiTeamController);
 
         //check controllers have been initialized
-        Assertions.assertNotNull(basicController);
-        Assertions.assertNotNull(contestController);
-        Assertions.assertNotNull(indiceController);
-        Assertions.assertNotNull(problemController);
+        assertNotNull(basicController);
+        assertNotNull(contestController);
+        assertNotNull(indiceController);
+        assertNotNull(problemController);
 
         //check repositories have been initialized
-        Assertions.assertNotNull(contestRepository);
-        Assertions.assertNotNull(problemDataRepository);
-        Assertions.assertNotNull(languageRepository);
-        Assertions.assertNotNull(problemRepository);
-        Assertions.assertNotNull(resultRepository);
-        Assertions.assertNotNull(submissionRepository);
-        Assertions.assertNotNull(teamRepository);
-        Assertions.assertNotNull(userRepository);
+        assertNotNull(contestRepository);
+        assertNotNull(problemDataRepository);
+        assertNotNull(languageRepository);
+        assertNotNull(problemRepository);
+        assertNotNull(resultRepository);
+        assertNotNull(submissionRepository);
+        assertNotNull(teamRepository);
+        assertNotNull(userRepository);
     }
 
     // verificar estado inicial de los repositorios
     @Test
     public void emptyRepositories() {
-        Assertions.assertEquals(contestRepository.findAll().size(), 1);
-        Assertions.assertTrue(problemDataRepository.findAll().isEmpty());
-        Assertions.assertFalse(languageRepository.findAll().isEmpty());
-        Assertions.assertTrue(problemRepository.findAll().isEmpty());
-        Assertions.assertTrue(resultRepository.findAll().isEmpty());
-        Assertions.assertTrue(submissionRepository.findAll().isEmpty());
-        Assertions.assertEquals(teamRepository.findAll().size(), 1);
-        Assertions.assertEquals(userRepository.findAll().size(), 1);
+        assertEquals(contestRepository.findAll().size(), 1);
+        assertTrue(problemDataRepository.findAll().isEmpty());
+        assertFalse(languageRepository.findAll().isEmpty());
+        assertTrue(problemRepository.findAll().isEmpty());
+        assertTrue(resultRepository.findAll().isEmpty());
+        assertTrue(submissionRepository.findAll().isEmpty());
+        assertEquals(teamRepository.findAll().size(), 1);
+        assertEquals(userRepository.findAll().size(), 1);
     }
 
     // verificar los lenguajes
     @Test
     public void languageTest() {
-        Assertions.assertNotNull(languageRepository.findLanguageByNombreLenguaje("java"));
-        Assertions.assertNotNull(languageRepository.findLanguageByNombreLenguaje("python"));
-        Assertions.assertNotNull(languageRepository.findLanguageByNombreLenguaje("c"));
-        Assertions.assertNotNull(languageRepository.findLanguageByNombreLenguaje("cpp"));
-        Assertions.assertNotNull(languageRepository.findLanguageByNombreLenguaje("sql"));
+        assertNotNull(languageRepository.findLanguageByNombreLenguaje("java"));
+        assertNotNull(languageRepository.findLanguageByNombreLenguaje("python"));
+        assertNotNull(languageRepository.findLanguageByNombreLenguaje("c"));
+        assertNotNull(languageRepository.findLanguageByNombreLenguaje("cpp"));
+        assertNotNull(languageRepository.findLanguageByNombreLenguaje("sql"));
     }
 
     // verificar el paso de mensajes por la cola -- rabbit
-    @Test
     public void testSend() {
 
     }
 
-    @Test
     public void testReceive() {
 
     }
-
-    //
-
 }
