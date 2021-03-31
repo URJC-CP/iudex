03f4bf02f3c1319ac0dc8cc2f2cb97b0ad503a9e
Sourius <sourius@users.noreply.github.com>
2021-03-31 16:24:19 +0200

    update test APISubmissionController

--- src/test/java/com/example/aplicacion/integrationtest/apiControllers/TestAPITeamController.java
+++ src/test/java/com/example/aplicacion/integrationtest/apiControllers/TestAPITeamController.java
@@ -1,7 +1,9 @@
 package com.example.aplicacion.integrationtest.apiControllers;
 
 import com.example.aplicacion.Controllers.apiControllers.APITeamController;
+import com.example.aplicacion.Entities.Team;
 import com.example.aplicacion.services.TeamService;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -13,4 +15,13 @@ public class TestAPITeamController {
 	private MockMvc mockMvc;
 	@MockBean
 	private TeamService teamService;
+
+	private Team team;
+
+	@BeforeEach
+	public void init() {
+		team = new Team();
+		team.setId(305);
+		team.setNombreEquipo("Equipo de prueba");
+	}
 }
