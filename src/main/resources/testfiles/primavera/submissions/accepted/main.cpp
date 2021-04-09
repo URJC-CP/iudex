20d4f170108622b91d4689fea6d0f825c9ea80b4
Sourius <sourius@users.noreply.github.com>
2021-04-09 18:46:48 +0200

    update gitignore: remove testfiles

--- src/main/resources/testfiles/primavera/submissions/accepted/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main(){
-    printf("Hola Abril");
-    return 0;
-}
--- src/main/resources/testfiles/primavera/submissions/accepted/main.java
+++
@@ -1,6 +0,0 @@
-class Isaac {
-    public static void main(String[] args) {
-
-            System.out.printf("Hola Abril");
-    }
-}
