37913da62d3771c411e8130babfd89334400465c
Sourius <sourius@users.noreply.github.com>
2021-04-10 17:31:18 +0200

    update app test: test add submission, test get submission

--- src/test/java/com/example/aplicacion/TheJudgeApplicationTests.java
+++ src/test/java/com/example/aplicacion/TheJudgeApplicationTests.java
@@ -21,12 +21,12 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest
 public class TheJudgeApplicationTests {
 	private final String baseURL = "http://localhost:8080/API/v1";
 	private final String basePathTestFiles = "/src/main/resources/testfiles";
-	private final String teamId = "6";
 	private RestTemplate restTemplate;
 	private ObjectMapper mapper;
 
@@ -38,7 +38,7 @@ public class TheJudgeApplicationTests {
 
 	@Test
 	@DisplayName("Verificar estado inicial de la aplicación")
-	public void testInitialState() throws IOException, JSONException, ParseException {
+	public void test1() throws IOException, JSONException, ParseException {
 		String url = baseURL + "/contest/?contestId=7";
 		ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
 		assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
@@ -56,8 +56,8 @@ public class TheJudgeApplicationTests {
 	}
 
 	@Test
-	@DisplayName("Crear concurso desde cero")
-	public void testCreateContestFromZero() throws IOException {
+	@DisplayName("Crear concurso con el usuario por defecto")
+	public void test2() throws IOException {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
@@ -101,35 +101,36 @@ public class TheJudgeApplicationTests {
 	//crear problemas
 	@Test
 	@DisplayName("Crear problema desde un archivo zip")
-	public void testCreateProblemFromZip() throws IOException {
-		String filename = "primavera.zip";
-		String problemName = "";
+	public void test3() throws IOException {
+		String teamId = "6";
 		String contesId = "7";
-
 		String badTeamId = "897";
 		String badContestId = "576";
 		String salida = "";
 
+		// valid file without problem name
+		String filename = "primavera.zip";
+		String problemName = "";
 		salida = "TEAM NOT FOUND";
 		testCreateProblemFromZipWithException(filename, problemName, badTeamId, badContestId, salida);
 
 		salida = "CONCURSO NOT FOUND";
 		testCreateProblemFromZipWithException(filename, problemName, teamId, badContestId, salida);
 
-		// empty problem name
 		testCreateProblemFromZip(filename, problemName, teamId, contesId);
 
-		// empty file
+		// unnamed empty file without problem name
 		filename = ".zip.zip";
 		salida = "Nombre del problema vacio";
 		testCreateProblemFromZipWithException(filename, problemName, teamId, contesId, salida);
 
+		// unnamed empty file with problem name
 		filename = ".zip.zip";
 		problemName = "pruba vacio";
 		salida = "No hay casos de prueba";
 		testCreateProblemFromZipWithException(filename, problemName, teamId, contesId, salida);
 
-		// file with problem name
+		// valid file with problem name
 		filename = "pruebaMYSQL.zip";
 		problemName = "problema de mysql";
 		testCreateProblemFromZip(filename, problemName, teamId, contesId);
@@ -181,11 +182,153 @@ public class TheJudgeApplicationTests {
 
 		HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(params, headers);
 		Exception exception = assertThrows(Exception.class, () -> {
-			ResponseEntity<String> response = restTemplate.postForEntity(createProblemURL, requestEntity, String.class);
+			restTemplate.postForEntity(createProblemURL, requestEntity, String.class);
 		});
 		assertThat(exception.getMessage(), equalTo("404 : [" + salida + "]"));
 	}
 
 	//realizar entregas
+	@Test
+	@DisplayName("Realizar entrega")
+	public void test4() throws IOException {
+		//fail("Not implemented yet!");
+		String contestId = "7";
+		String anotherContestId = "8";
+		String badContestId = "768";
+		String problemPrimavera = "1";
+		String problemMySQL = "2";
+		String badProblemId = "874";
+		String teamId = "6";
+		String badTeamId = "987";
 
+		String language = "";
+		String codeFile = "vacio.java";
+		String salida = "CONTEST NOT FOUND";
+		testAddSubmissionWithException(badContestId, badProblemId, badTeamId, language, codeFile, salida);
+
+		salida = "PROBLEM NOT FOUND";
+		testAddSubmissionWithException(contestId, badProblemId, badTeamId, language, codeFile, salida);
+
+		salida = "TEAM NOT FOUND";
+		testAddSubmissionWithException(contestId, problemPrimavera, badTeamId, language, codeFile, salida);
+
+		//salida = "LANGUAGE NOT FOUND"; --> cannot be reached --> 500 : [ERROR GENERAL DEL SISTEMA]
+		//language = getLanguage("php");
+		//testAddSubmissionWithException(contestId, problemPrimavera, teamId, language, codeFile, salida);
+
+		language = getLanguage("java");
+		salida = "PROBLEM NOT IN CONCURSO";
+		testAddSubmissionWithException(anotherContestId, problemPrimavera, teamId, language, codeFile, salida);
+
+		// incorrect language selected
+		codeFile = "primavera/submissions/accepted/main.java";
+		language = getLanguage("python");
+		testAddSubmission(contestId, problemPrimavera, teamId, language, codeFile);
+
+		// all okay
+		codeFile = "primavera/submissions/accepted/main.java";
+		language = getLanguage("java");
+		testAddSubmission(contestId, problemPrimavera, teamId, language, codeFile);
+
+		// incorrect file type
+		codeFile = ".zip.zip";
+		language = getLanguage("java");
+		testAddSubmission(contestId, problemPrimavera, teamId, language, codeFile);
+
+		fail("I created a python submission with a java file" +
+			"\nI created a submission with an empty zip file");
+	}
+
+	private String getLanguage(String language) {
+		language = language.toLowerCase().trim();
+		switch (language) {
+			case "java":
+				return "1";
+			case "python":
+				return "2";
+			case "c":
+				return "2";
+			case "cpp":
+			case "c++":
+				return "3";
+			case "sql":
+				return "4";
+			default:
+				return "1023";
+		}
+	}
+
+	private void testAddSubmission(String contestId, String problemId, String teamId, String language, String codeFileName) throws IOException {
+		String addSubmissionURL = baseURL + "/submission";
+		File file = new File("." + basePathTestFiles + "/" + codeFileName);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+		MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
+		params.add("problemId", problemId);
+		params.add("contestId", contestId);
+		params.add("codigo", new FileSystemResource(file));
+		params.add("lenguaje", language);
+		params.add("teamId", teamId);
+
+		HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(params, headers);
+		ResponseEntity<String> response = restTemplate.postForEntity(addSubmissionURL, requestEntity, String.class);
+		assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+		System.out.println(response.getBody());
+
+		JsonNode submission = mapper.readTree(response.getBody());
+		String submissionId = submission.get("id").asText();
+		testGetSubmission(submissionId);
+	}
+
+	private void testAddSubmissionWithException(String contestId, String problemId, String teamId, String language, String codeFileName, String salida) {
+		String addSubmissionURL = baseURL + "/submission";
+		File file = new File("." + basePathTestFiles + "/" + codeFileName);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+		MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
+		params.add("problemId", problemId);
+		params.add("contestId", contestId);
+		params.add("codigo", new FileSystemResource(file));
+		params.add("lenguaje", language);
+		params.add("teamId", teamId);
+
+		HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(params, headers);
+		Exception exception = assertThrows(Exception.class, () -> {
+			restTemplate.postForEntity(addSubmissionURL, requestEntity, String.class);
+		});
+		assertThat(exception.getMessage(), equalTo("404 : [" + salida + "]"));
+	}
+
+	@Test
+	@DisplayName("Get a submission")
+	public void test5() throws IOException {
+		String badSubId = "756";
+		String subId = "6";
+
+		String salida = "SUBMISSION NOT FOUND";
+		testGetSubmissionWithException(badSubId, salida);
+		testGetSubmission(subId);
+	}
+
+	private void testGetSubmission(String subId) throws IOException {
+		String getSubmissionURL = baseURL + "/submission/" + subId;
+		ResponseEntity<String> response = restTemplate.getForEntity(getSubmissionURL, String.class);
+		assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+		System.out.println(response.getBody());
+
+		JsonNode submission = mapper.readTree(response.getBody());
+		assertThat(submission.get("id").asText(), equalTo(subId));
+	}
+
+	private void testGetSubmissionWithException(String subId, String salida) {
+		String getSubmissionURL = baseURL + "/submission/" + subId;
+		Exception exception = assertThrows(Exception.class, () -> {
+			restTemplate.getForEntity(getSubmissionURL, String.class);
+		});
+		assertThat(exception.getMessage(), equalTo("404 : [" + salida + "]"));
+	}
 }
