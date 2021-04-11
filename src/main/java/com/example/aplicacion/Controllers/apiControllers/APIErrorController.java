2eed9fafa6e2987975dc1f27046ee65a8883869a
Raúl Martín <raul.martin@urjc.es>
2021-04-12 00:00:42 +0200

    CORS Config for RestControllers via @CrossOrigin

--- src/main/java/com/example/aplicacion/Controllers/apiControllers/APIErrorController.java
+++ src/main/java/com/example/aplicacion/Controllers/apiControllers/APIErrorController.java
@@ -3,11 +3,10 @@ package com.example.aplicacion.Controllers.apiControllers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.*;
 
 @RestControllerAdvice
+@CrossOrigin(methods = {RequestMethod.DELETE, RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT})
 public class APIErrorController {
 
     Logger logger = LoggerFactory.getLogger(APIErrorController.class);
--- src/main/java/com/example/aplicacion/Controllers/apiControllers/APIProblemController.java
+++ src/main/java/com/example/aplicacion/Controllers/apiControllers/APIProblemController.java
@@ -20,6 +20,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(methods = {RequestMethod.DELETE, RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT})
 public class APIProblemController {
 
     @Autowired
