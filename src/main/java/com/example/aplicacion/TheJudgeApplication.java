185f94ebc2259c06b9683e4cc8b7219d614e1491
Sourius <sourius@users.noreply.github.com>
2021-06-24 18:03:25 +0200

    remove hello world and update properties

--- src/main/java/com/example/aplicacion/TheJudgeApplication.java
+++ src/main/java/com/example/aplicacion/TheJudgeApplication.java
@@ -14,7 +14,6 @@ public class TheJudgeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TheJudgeApplication.class, args);
-        //new DockerHelloWorld();
         new ResultHandler();
     }
 
