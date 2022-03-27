15fc1d9cb4b785e0fbcb7eade39b36a303c4cb42
Raúl Martín <raul.martin@urjc.es>
2022-03-27 23:50:07 +0200

    Delete unused dependency in SmokeTest, add javax as maven dependency instead of lib folder

--- src/test/java/com/example/aplicacion/smoketest/SmokeTest.java
+++ src/test/java/com/example/aplicacion/smoketest/SmokeTest.java
@@ -18,7 +18,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
