80d580cbbe34a14c9de961b85e587fd28cb19ced
rufino <rufinogsnchz@gmail.com>
2022-06-08 15:31:00 +0200

    delete @service

--- src/main/java/es/urjc/etsii/grafo/iudex/services/events/JudgeEventListener.java
+++ src/main/java/es/urjc/etsii/grafo/iudex/services/events/JudgeEventListener.java
@@ -3,7 +3,6 @@ package es.urjc.etsii.grafo.iudex.services.events;
 
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Service;
 
 import java.lang.annotation.*;
 
@@ -12,7 +11,6 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Async
 @EventListener
-@Service
 public @interface JudgeEventListener {
 
 }
