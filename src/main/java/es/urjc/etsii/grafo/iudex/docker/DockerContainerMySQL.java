c26747e56a131733791aaed60ad0bfefe3f5115b
xIvqn <xivqn@proton.me>
2023-06-22 23:29:21 +0200

    fix: cerrar CreateContainerCmd
    Al igual que en 04cb5d , se ha incluido el código que utiliza el contenedor dentro de un try con recursos siguiendo la regla java:S2095 de SonarCloud.io.

--- src/main/java/es/urjc/etsii/grafo/iudex/docker/DockerContainerMySQL.java
+++ src/main/java/es/urjc/etsii/grafo/iudex/docker/DockerContainerMySQL.java
@@ -1,5 +1,6 @@
 package es.urjc.etsii.grafo.iudex.docker;
 
+import com.github.dockerjava.api.command.CreateContainerCmd;
 import es.urjc.etsii.grafo.iudex.entities.Result;
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.command.CreateContainerResponse;
@@ -35,47 +36,60 @@ public class DockerContainerMySQL extends DockerContainer {
         hostConfig.withMemory(Long.parseLong(defaultMemoryLimit)).withCpusetCpus(defaultCPU);
 
         DockerClient dockerClient = getDockerClient();
-        CreateContainerResponse container = dockerClient.createContainerCmd(imagenId).withNetworkDisabled(true).withEnv("EXECUTION_TIMEOUT=" + timeout, "FILENAME1=entrada.in", "FILENAME2=" + nombreClase + ".sql").withHostConfig(hostConfig).withName(nombreDocker).exec();
-        logger.debug("DOCKER MySQL: Running container for result {} with timeout {} and memory limit {}", result.getId(), timeout, result.getMaxMemory());
+        try (CreateContainerCmd createContainerCmd = dockerClient.createContainerCmd(imagenId)
+                .withNetworkDisabled(true)
+                .withEnv("EXECUTION_TIMEOUT=" + timeout, "FILENAME1=entrada.in", "FILENAME2=" + nombreClase + ".sql")
+                .withHostConfig(hostConfig).withName(nombreDocker)) {
+            CreateContainerResponse container = createContainerCmd.exec();
+            logger.debug("DOCKER MySQL: Running container for result {} with timeout {} and memory limit {}", result.getId(), timeout, result.getMaxMemory());
 
-        //Copiamos el codigo
-        copiarArchivoAContenedor(container.getId(), nombreClase + ".sql", result.getCodigo(), "/root");
+            //Copiamos el codigo
+            copiarArchivoAContenedor(container.getId(), nombreClase + ".sql", result.getCodigo(), "/root");
 
-        //Copiamos la entrada
-        copiarArchivoAContenedor(container.getId(), "entrada.in", result.getEntrada(), "/root");
+            //Copiamos la entrada
+            copiarArchivoAContenedor(container.getId(), "entrada.in", result.getEntrada(), "/root");
 
-        //Arrancamos el docker
-        dockerClient.startContainerCmd(container.getId()).exec();
-        // TODO chapucero, hardcodeado esperar 10 segundos a que este la bbdd lista, deberia ser cuando este el puerto 3306 ok
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            logger.error("context", e);
+            //Arrancamos el docker
+            dockerClient.startContainerCmd(container.getId()).exec();
+            // TODO chapucero, hardcodeado esperar 10 segundos a que este la bbdd lista, deberia ser cuando este el puerto 3306 ok
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                logger.error("context", e);
+            }
+
+            var executionID = dockerClient.execCreateCmd(container.getId()).withAttachStdout(true).withCmd("bash", "-c", "mysql -h localhost -u$MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE <$FILENAME1 >salidaError.ans 2>&1 && mysql -h localhost -u$MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE <$FILENAME2 >salidaEstandar.ans 2>salidaError.ans; echo $? >>signalEjecutor.txt").exec().getId();
+            // Starting the execution
+            try {
+                dockerClient.execStartCmd(executionID).exec(new ExecStartResultCallback(System.out, System.err)).awaitCompletion();
+            } catch (InterruptedException e) {
+                logger.error("context", e);
+            }
+
+            LocalDateTime maxTime = LocalDateTime.now().plusSeconds(Long.parseLong(result.getMaxTimeout()));
+            String signalEjecutor = null;
+            do {
+                Thread.onSpinWait();
+                signalEjecutor = copiarArchivoDeContenedor(container.getId(), "root/signalEjecutor.txt");
+            } while (LocalDateTime.now().isBefore(maxTime) && signalEjecutor.equals(""));
+
+            dockerClient.stopContainerCmd(container.getId()).exec();
+            //comprueba el estado del contenedor y no sigue la ejecucion hasta que este esta parado
+            Boolean isRunning;
+            do {
+                isRunning = dockerClient.inspectContainerCmd(container.getId()).exec().getState().getRunning();
+            } while (isRunning != null && isRunning);  //Mientras esta corriendo se hace el do
+
+            this.setReturnedValuesFromContainer(container, result, signalEjecutor);
+
+            dockerClient.removeContainerCmd(container.getId()).withRemoveVolumes(true).exec();
         }
 
-        var executionID = dockerClient.execCreateCmd(container.getId()).withAttachStdout(true).withCmd("bash", "-c", "mysql -h localhost -u$MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE <$FILENAME1 >salidaError.ans 2>&1 && mysql -h localhost -u$MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE <$FILENAME2 >salidaEstandar.ans 2>salidaError.ans; echo $? >>signalEjecutor.txt").exec().getId();
-        // Starting the execution
-        try {
-            dockerClient.execStartCmd(executionID).exec(new ExecStartResultCallback(System.out, System.err)).awaitCompletion();
-        } catch (InterruptedException e) {
-            logger.error("context", e);
-        }
+        logger.debug("DOCKER MySQL: Finish running container for result {} ", result.getId());
+        return result;
+    }
 
-        LocalDateTime maxTime = LocalDateTime.now().plusSeconds(Long.parseLong(result.getMaxTimeout()));
-        String signalEjecutor = null;
-        do {
-            Thread.onSpinWait();
-            signalEjecutor = copiarArchivoDeContenedor(container.getId(), "root/signalEjecutor.txt");
-        } while (LocalDateTime.now().isBefore(maxTime) && signalEjecutor.equals(""));
-
-        dockerClient.stopContainerCmd(container.getId()).exec();
-
-        //comprueba el estado del contenedor y no sigue la ejecucion hasta que este esta parado
-        Boolean isRunning = null;
-        do {
-            isRunning = dockerClient.inspectContainerCmd(container.getId()).exec().getState().getRunning();
-        } while (isRunning != null && isRunning);  //Mientras esta corriendo se hace el do
-
+    private void setReturnedValuesFromContainer(CreateContainerResponse container, Result result, String signalEjecutor) throws IOException {
         //Buscamos la salida Estandar
         String salidaEstandar = copiarArchivoDeContenedor(container.getId(), "root/salidaEstandar.ans");
         result.setSalidaEstandar(salidaEstandar);
@@ -90,10 +104,5 @@ public class DockerContainerMySQL extends DockerContainer {
         //para que no de fallo de compilador
         result.setSignalCompilador("0");
         result.setSignalEjecutor(signalEjecutor);
-
-        dockerClient.removeContainerCmd(container.getId()).withRemoveVolumes(true).exec();
-
-        logger.debug("DOCKER MySQL: Finish running container for result {} ", result.getId());
-        return result;
     }
 }
