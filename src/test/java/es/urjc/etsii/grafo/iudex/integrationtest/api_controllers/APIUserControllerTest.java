f5b924eb500ebdee54c29664c90a1dceebe3f187
xIvqn <xivqn@proton.me>
2023-07-09 13:38:01 +0200

    feat: test create user con salida 200

---
+++ src/test/java/es/urjc/etsii/grafo/iudex/integrationtest/api_controllers/APIUserControllerTest.java
@@ -0,0 +1,52 @@
+package es.urjc.etsii.grafo.iudex.integrationtest.api_controllers;
+
+import es.urjc.etsii.grafo.iudex.api.v1.APIUserController;
+import es.urjc.etsii.grafo.iudex.entities.User;
+import es.urjc.etsii.grafo.iudex.pojos.UserString;
+import es.urjc.etsii.grafo.iudex.services.UserAndTeamService;
+import es.urjc.etsii.grafo.iudex.utils.JSONConverter;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(APIUserController.class)
+class APIUserControllerTest {
+
+    private final JSONConverter jsonConverter = new JSONConverter();
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    UserAndTeamService userService;
+
+    @Test
+    void createUser() throws Exception {
+        String url = "/API/v1/user";
+
+        User user = new User("iudex", "iudex@example.org");
+
+        UserString userString = new UserString();
+        userString.setUser(user);
+        userString.setSalida("OK");
+
+        when(userService.crearUsuario(user.getNickname(), user.getEmail())).thenReturn(userString);
+
+        String result = mockMvc.perform(post(url)
+                        .param("username", user.getNickname())
+                        .param("email", user.getEmail()))
+                .andExpect(status().is(200)).andDo(print()).andReturn().getResponse().getContentAsString();
+        assertEquals(jsonConverter.convertObjectToJSON(user.toUserAPI()), result);
+    }
+
+}
