4728023c5789d6983791c2c833bb8991fc0c68b5
xIvqn <xivqn@proton.me>
2023-07-23 19:37:20 +0200

    fix: constructors in DockerExecutionException

--- src/main/java/es/urjc/etsii/grafo/iudex/exceptions/DockerExecutionException.java
+++ src/main/java/es/urjc/etsii/grafo/iudex/exceptions/DockerExecutionException.java
@@ -1,3 +1,20 @@
 package es.urjc.etsii.grafo.iudex.exceptions;
 
-public class DockerExecutionException extends RuntimeException { }
+public class DockerExecutionException extends RuntimeException {
+
+    public DockerExecutionException() {
+    }
+
+    public DockerExecutionException(String message) {
+        super(message);
+    }
+
+    public DockerExecutionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DockerExecutionException(Throwable cause) {
+        super(cause);
+    }
+
+}
