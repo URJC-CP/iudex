6b81ed39f7daadeb992ad8fe02371aa76554230a
xIvqn <xivqn@proton.me>
2023-08-21 13:45:53 +0200

    fix: return not found while trying to remove a not owned role

--- src/main/java/es/urjc/etsii/grafo/iudex/api/v1/APIUserController.java
+++ src/main/java/es/urjc/etsii/grafo/iudex/api/v1/APIUserController.java
@@ -51,7 +51,7 @@ public class APIUserController {
         if (!List.of("ROLE_ADMIN", "ROLE_JUDGE", "ROLE_USER").contains(role)) return ResponseEntity.badRequest().build();
 
         User user = optionalUser.get();
-        if (!user.getRoles().contains(role)) return ResponseEntity.badRequest().build();
+        if (!user.getRoles().contains(role)) return ResponseEntity.notFound().build();
         else user = userAndTeamService.removeRoleFromUser(role, user);
 
         return ResponseEntity.ok(user.getRoles());
