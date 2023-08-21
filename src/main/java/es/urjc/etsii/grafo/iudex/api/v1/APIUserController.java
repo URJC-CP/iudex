9d31f4454b914eb59505d269fc4a0bcbb38974fe
xIvqn <xivqn@proton.me>
2023-08-21 12:00:26 +0200

    feat: add and remove roles from users

--- src/main/java/es/urjc/etsii/grafo/iudex/api/v1/APIUserController.java
+++ src/main/java/es/urjc/etsii/grafo/iudex/api/v1/APIUserController.java
@@ -1,5 +1,6 @@
 package es.urjc.etsii.grafo.iudex.api.v1;
 
+import es.urjc.etsii.grafo.iudex.entities.User;
 import es.urjc.etsii.grafo.iudex.pojos.UserAPI;
 import es.urjc.etsii.grafo.iudex.pojos.UserString;
 import es.urjc.etsii.grafo.iudex.services.UserAndTeamService;
@@ -11,24 +12,42 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @CrossOrigin(methods = {RequestMethod.POST})
 public class APIUserController {
+
     @Autowired
-    UserAndTeamService userService;
+    UserAndTeamService userAndTeamService;
 
-    @Operation( summary = "Creates a User")
-    @PostMapping("/API/v1/user")
+    @Operation( summary = "Add a specific role to an existing user")
+    @PostMapping("/API/v1/user/{id}/role")
     public ResponseEntity<UserAPI> createUser(@RequestParam String username, @RequestParam String email) {
-        username = Sanitizer.removeLineBreaks(username);
-        email = Sanitizer.removeLineBreaks(email);
+    public ResponseEntity<List<String>> addRoleToUser(@PathVariable long id, @RequestParam String role) {
+        Optional<User> optionalUser = userAndTeamService.getUserById(id);
+        if (optionalUser.isEmpty()) return ResponseEntity.notFound().build();
 
-        UserString salida = userService.crearUsuario(username, email);
-        if (salida.getSalida().equals("OK")) {
-            return new ResponseEntity<>(salida.getUser().toUserAPI(), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        role = "ROLE_" + role.toUpperCase();
+        User user = optionalUser.get();
+        if (!user.getRoles().contains(role)) user.getRoles().add(role);
+
+        return ResponseEntity.ok(user.getRoles());
+    }
+
+    @Operation( summary = "Remove a role from an existing user")
+    @PostMapping("/API/v1/user/{id}/role/{role}")
+    public ResponseEntity<List<String>> removeRoleToUser(@PathVariable long id, @PathVariable String role) {
+        Optional<User> optionalUser = userAndTeamService.getUserById(id);
+        if (optionalUser.isEmpty()) return ResponseEntity.notFound().build();
+
+        role = "ROLE_" + role.toUpperCase();
+        User user = optionalUser.get();
+        if (!user.getRoles().contains(role)) return ResponseEntity.badRequest().build();
+        else user.getRoles().remove(role);
+
+        return ResponseEntity.ok(user.getRoles());
     }
 
 }
