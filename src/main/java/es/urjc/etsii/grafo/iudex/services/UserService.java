3ca38f5391cf1075f0e85ef820baba20c86d7693
xIvqn <xivqn@proton.me>
2023-08-23 10:39:08 +0200

    fix: check for oauth user properties missing

--- src/main/java/es/urjc/etsii/grafo/iudex/services/UserService.java
+++ src/main/java/es/urjc/etsii/grafo/iudex/services/UserService.java
@@ -11,8 +11,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Service;
 
+import javax.validation.constraints.Null;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -30,18 +32,23 @@ public class UserService {
     public User getUserFromOAuthPrincipal(OidcUser oAuth2User) {
         Optional<User> user = userRepository.findUserByEmail(oAuth2User.getAttribute("email"));
 
-        if (user.isEmpty()) throw new IudexException("User not registered");
+        return user.orElse(null);
 
-        return user.get();
     }
 
     public User signupUserFromOAuthPrincipal(OidcUser oAuth2User) {
-        User user = new User(
-                oAuth2User.getAttribute("preferred_username").toString(),
-                oAuth2User.getAttribute("email").toString(),
-                oAuth2User.getAttribute("given_name").toString(),
-                oAuth2User.getAttribute("family_name").toString()
-        );
+        User user;
+
+        try {
+            user = new User(
+                    Objects.requireNonNull(oAuth2User.getAttribute("preferred_username")).toString(),
+                    Objects.requireNonNull(oAuth2User.getAttribute("email")).toString(),
+                    Objects.requireNonNull(oAuth2User.getAttribute("given_name")).toString(),
+                    Objects.requireNonNull(oAuth2User.getAttribute("family_name")).toString()
+            );
+        } catch (NullPointerException e) {
+            throw new IudexException("OAuth2 account has required values missing");
+        }
 
         List<String> roles = new ArrayList<>();
         oAuth2User.getAuthorities().forEach(authority -> roles.add(authority.getAuthority()));
@@ -57,11 +64,18 @@ public class UserService {
     }
 
     public AuthResponse loginUser(OidcUser oidcUser) {
-        User user;
-        try {
-            user = getUserFromOAuthPrincipal(oidcUser);
-        } catch (IudexException e) {
-            user = signupUserFromOAuthPrincipal(oidcUser);
+        User user = getUserFromOAuthPrincipal(oidcUser);
+
+        if (user == null) {
+            try {
+                user = signupUserFromOAuthPrincipal(oidcUser);
+            } catch (IudexException e) {
+                return new AuthResponse(
+                        AuthResponse.Status.FAILURE,
+                        "Iudex account not found and an error has occurred while trying to sign up",
+                        e.getMessage()
+                );
+            }
         }
 
         UserDetails userDetails = userDetailsServiceImp.loadUserByUsername(user.getNickname());
