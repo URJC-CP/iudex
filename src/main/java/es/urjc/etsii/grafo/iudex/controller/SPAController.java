8b39edebb1d7f1f9004b20491a5a73cbb3e7fcc3
Adaya:) <103607990+AdayaUwU@users.noreply.github.com>
2023-09-29 12:09:08 +0200

    Add SPAController

---
+++ src/main/java/es/urjc/etsii/grafo/iudex/controller/SPAController.java
@@ -0,0 +1,13 @@
+package es.urjc.etsii.grafo.iudex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SPAController {
+    @RequestMapping( "/{path:web[^\\.]*}")
+    public String redirect() {
+        return "forward:/web/index.html";
+    }
+}
