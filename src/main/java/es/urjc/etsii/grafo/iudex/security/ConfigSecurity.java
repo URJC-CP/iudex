c195342136e1246322ef9cac49734604c7a5324c
Iván <72264031+xIvqn@users.noreply.github.com>
2023-09-30 19:32:13 +0200

    fix: security filter chain deprecated methods

--- src/main/java/es/urjc/etsii/grafo/iudex/security/ConfigSecurity.java
+++ src/main/java/es/urjc/etsii/grafo/iudex/security/ConfigSecurity.java
@@ -2,7 +2,8 @@ package es.urjc.etsii.grafo.iudex.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -10,19 +11,18 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(jsr250Enabled=true)
+@EnableMethodSecurity(jsr250Enabled=true)
 class ConfigSecurity {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                .antMatchers("/API/v1/oauth/**")
-                .authenticated()
-                .and()
-                .oauth2Login();
+        http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/API/v1/oauth/**")
+                        .authenticated())
+                .oauth2Login(Customizer.withDefaults());
 
         // Disable CORS and CSRF protection
-        http.cors().and().csrf().disable();
+        http.cors(AbstractHttpConfigurer::disable).csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
