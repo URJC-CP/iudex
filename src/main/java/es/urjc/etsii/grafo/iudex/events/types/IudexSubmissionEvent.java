bd092ace3e88002336cfe5185bf8bdaf02247dd9
Iván <72264031+xIvqn@users.noreply.github.com>
2023-09-30 11:39:55 +0200

    fix: event constructor for sample submissions

--- src/main/java/es/urjc/etsii/grafo/iudex/events/types/IudexSubmissionEvent.java
+++ src/main/java/es/urjc/etsii/grafo/iudex/events/types/IudexSubmissionEvent.java
@@ -4,7 +4,10 @@ import es.urjc.etsii.grafo.iudex.pojos.SubmissionAPI;
 
 public class IudexSubmissionEvent implements IudexEvent {
 
-    private final SubmissionAPI submission;
+    private SubmissionAPI submission;
+
+    public IudexSubmissionEvent() {
+    }
 
     public IudexSubmissionEvent(SubmissionAPI submission) {
         this.submission = submission;
@@ -12,5 +15,9 @@ public class IudexSubmissionEvent implements IudexEvent {
 
     public SubmissionAPI getSubmission() {
         return submission;
+    }
+
+    public void setSubmission(SubmissionAPI submission) {
+        this.submission = submission;
     }
 }
