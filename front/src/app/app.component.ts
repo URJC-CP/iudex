ea7559e21a3706d662da7233e795fa3fb4084a97
Adaya:) <103607990+AdayaUwU@users.noreply.github.com>
2023-10-16 09:11:01 +0200

    Add PrimeNG styles and fix internationalization

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TagModule } from 'primeng/tag';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['../styles.css']
 })
 export class AppComponent {
   title = 'IUDEX';
