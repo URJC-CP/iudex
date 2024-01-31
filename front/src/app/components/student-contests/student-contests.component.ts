9ddb8e94ac1b6e286741a6d672cc93e7570e7e0f
Adaya:) <103607990+AdayaUwU@users.noreply.github.com>
2024-01-31 16:40:36 +0100

    Get data from API in student contests component

--- front/src/app/components/student-contests/student-contests.component.ts
+++ front/src/app/components/student-contests/student-contests.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ContestDTO } from 'src/app/dto/contest.dto';
+import { ContestService } from 'src/app/services/contest.service';
 
 @Component({
   selector: 'app-student-contests',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['../../../styles.css']
 })
 export class StudentContestsComponent {
+  
+  allContests: ContestDTO[] | undefined;
+  forStart: number | undefined;
+  forEnd: number | undefined;
 
+  constructor(private contestService: ContestService, private router: Router) {
+
+  }
+
+  ngOnInit(){
+    this.contestService.getAllContests().subscribe((contests) => {
+      this.allContests = contests;
+    });
+  }
+
+  redirect(id: string){
+    this.router.navigate(['/student/contest/' + id]);
+  }
 }
