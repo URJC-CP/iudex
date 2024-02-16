95488f7b39fb8f3186f35f15babda7ddf469e452
Adaya:) <103607990+AdayaUwU@users.noreply.github.com>
2024-02-16 11:23:43 +0100

    WIP permit all annotation

--- src/main/java/es/urjc/etsii/grafo/iudex/controller/SPAController.java
+++ src/main/java/es/urjc/etsii/grafo/iudex/controller/SPAController.java
@@ -1,10 +1,12 @@
 package es.urjc.etsii.grafo.iudex.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SPAController {
+    @PreAuthorize("permitAll()")
     @RequestMapping( "/{path:[^\\.]*}")
     public String redirect() {
         return "forward:/index.html";
