034fa8a843b5aee7373f400b75dd3de4b5ac0db7
Iván <72264031+xIvqn@users.noreply.github.com>
2024-07-18 15:53:16 +0200

    fix: mapped by column

--- src/main/java/es/urjc/etsii/grafo/iudex/entities/Submission.java
+++ src/main/java/es/urjc/etsii/grafo/iudex/entities/Submission.java
@@ -20,7 +20,7 @@ public class Submission {
     private String filename;
     @ManyToOne
     private Problem problem;
-    @OneToMany(cascade = CascadeType.ALL)
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "submission", fetch = FetchType.EAGER)
     private Set<Result> results;
 
     private boolean corregido;
