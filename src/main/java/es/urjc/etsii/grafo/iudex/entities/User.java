f1bd1bd6fee2bf88f02c14a2cd5618a866d420e4
Iván <72264031+xIvqn@users.noreply.github.com>
2024-07-18 15:44:57 +0200

    fix: filter SCOPE_ roles

--- src/main/java/es/urjc/etsii/grafo/iudex/entities/User.java
+++ src/main/java/es/urjc/etsii/grafo/iudex/entities/User.java
@@ -58,7 +58,7 @@ public class User {
         userAPI.setName(this.name);
         userAPI.setEmail(this.email);
         userAPI.setTimestamp(this.timestamp);
-        userAPI.setRoles(new ArrayList<>(this.roles));
+        userAPI.setRoles(new ArrayList<>(this.roles).stream().filter(r -> r.startsWith("ROLE_")).toList());
         return userAPI;
     }
 
