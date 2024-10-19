e91fa4f4c17e969fc2fb1c08db44e4ab67944114
Iván <72264031+xIvqn@users.noreply.github.com>
2024-10-19 14:55:03 +0200

    feat: load user data on account creation
    only for demo deployment only, revert after that

--- src/main/java/es/urjc/etsii/grafo/iudex/services/UserService.java
+++ src/main/java/es/urjc/etsii/grafo/iudex/services/UserService.java
@@ -1,8 +1,8 @@
 package es.urjc.etsii.grafo.iudex.services;
 
-import es.urjc.etsii.grafo.iudex.entities.User;
+import es.urjc.etsii.grafo.iudex.entities.*;
 import es.urjc.etsii.grafo.iudex.exceptions.IudexException;
-import es.urjc.etsii.grafo.iudex.repositories.UserRepository;
+import es.urjc.etsii.grafo.iudex.repositories.*;
 import es.urjc.etsii.grafo.iudex.security.AuthResponse;
 import es.urjc.etsii.grafo.iudex.security.JwtTokenProvider;
 import es.urjc.etsii.grafo.iudex.security.UserDetailsServiceImp;
@@ -10,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -21,16 +22,33 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    private final TeamRepository teamRepository;
+    private final UserTeamRespository userTeamRespository;
+    private final ContestRepository contestRepository;
+    private final ContestTeamRespository contestTeamRespository;
+
+    private final UserAndTeamService userAndTeamService;
+    private final ContestService contestService;
+    private final ContestTeamService contestTeamService;
+
     private final UserDetailsServiceImp userDetailsServiceImp;
 
     private final JwtTokenProvider jwtTokenProvider;
 
     private final Map<String, AuthResponse> pendingLogins = new ConcurrentHashMap<>();
 
-    public UserService(UserRepository userRepository, UserDetailsServiceImp userDetailsServiceImp, JwtTokenProvider jwtTokenProvider) {
+    public UserService(UserRepository userRepository, UserDetailsServiceImp userDetailsServiceImp, JwtTokenProvider jwtTokenProvider, TeamRepository teamRepository, UserTeamRespository userTeamRespository, ContestRepository contestRepository, ContestTeamRespository contestTeamRespository, UserAndTeamService userAndTeamService, ContestService contestService, ContestTeamService contestTeamService) {
         this.userRepository = userRepository;
         this.userDetailsServiceImp = userDetailsServiceImp;
         this.jwtTokenProvider = jwtTokenProvider;
+        this.teamRepository = teamRepository;
+        this.userTeamRespository = userTeamRespository;
+        this.contestRepository = contestRepository;
+        this.contestTeamRespository = contestTeamRespository;
+        this.userAndTeamService = userAndTeamService;
+        this.contestService = contestService;
+        this.contestTeamService = contestTeamService;
+
     }
 
     public User getUserFromOAuthPrincipal(OAuth2User oAuth2User) {
@@ -59,9 +77,21 @@ public class UserService {
             throw new IudexException("User already exists");
         }
 
-        userRepository.save(user);
+        frontPresentationDeployChanges(user);
 
         return user;
+    }
+
+    private void frontPresentationDeployChanges(User user) {
+        user.setRoles(List.of("ROLE_USER", "ROLE_JUDGE", "ROLE_ADMIN"));
+        userRepository.save(user);
+
+        userAndTeamService.crearTeam(user.getNickname(), true);
+        Team team = userAndTeamService.getTeamByNick(user.getNickname()).orElseThrow();
+        userAndTeamService.addUserToTeam(team, user);
+
+        Contest contest = contestService.getContestByName("contestPrueba").orElseThrow();
+        contestService.addTeamToContest(String.valueOf(contest.getId()), String.valueOf(team.getId()));
     }
 
     public String prepareForLogin(OAuth2User oAuth2User) {
